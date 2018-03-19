fdb39205680a4a0c8a449c5bb08ad8131c48e92d
Donutttt <orr2@live.com>
2018-03-19 16:23:07 +0000

    added guild api function

--- herald/src/api/index.js
+++ herald/src/api/index.js
@@ -17,11 +17,19 @@ const buildUrl = url => `${config.baseUrl}${url}`;
 const getFromApi = url => axios.get(buildUrl(url))
 
 /**
- * Gets a promise of playaer data based on the name provided.
+ * Gets a promise of player data based on the name provided.
  * @param {string} playerName the name of the player for which data is required
  * @returns {promise} returns data from the api once available.
  */
-export const getPlayer = (playerName) => {
+export const getPlayer = playerName => {
   return getFromApi(`player/${playerName}`);
 };
 
+/**
+ * Gets a promise of guild data based on the name provided.
+ * @param {string} guildName the name of the guild for which data is required
+ * @returns {promise} returns data from the api once available.
+ */
+export const getGuild = guildName => {
+  return getFromApi(`guild/${guildName}`);
+};
--- herald/src/base/index.js
+++ herald/src/base/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { getPlayer } from '../api/index';
-
-console.log('test');
+import { getPlayer, getGuild } from '../api/index';
 
 getPlayer('donut').then(data => console.dir(data));
+getGuild('inc').then(data => console.dir(data));
 
 ReactDOM.render(
   <p>Hello, world!</p>,
